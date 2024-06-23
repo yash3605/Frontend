d12916d6924a92f526c1e765d83150a3db47c3e5
YashPK <pratapyashkulshrestha@gmail.com>
2024-06-23 16:52:08 +0530

    project 07

---
+++ project07/src/app/store.js
@@ -0,0 +1,8 @@
+// step -1
+import { configureStore } from '@reduxjs/toolkit';
+import todoReducer from '../features/Todo/todoSlice';
+
+
+export const store = configureStore({
+    reducer: todoReducer
+})
